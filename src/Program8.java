665030cda5e6172e67279585bd8dde28c5af40e1
deepalibhujbal26 <deepalibhujbal26@gmail.com>
2023-08-23 17:01:58 -0400

    Modify For loop

--- src/Program8.java
+++ src/Program8.java
@@ -14,17 +14,18 @@ public class Program8 {
                 number[row] = scanner.nextInt();
             }
             int min = number[0];
+            int max = number[1];
+
             for (int i = 0; i < size; i++) {
                 if (number[i] < min) {
                     min = number[i];
                 }
-            }
-            int max = number[1];
-            for (int i = 0; i < size; i++) {
-                if (number[i] > max) {
-                    max = number[i];
+                else  if (number[i]>max)
+                {
+                    max=number[i];
                 }
             }
+
             System.out.println("The Final Output:");
             System.out.println("The Minimum number in given array " + min);
             System.out.println("The Maximum number in given array " + max);
