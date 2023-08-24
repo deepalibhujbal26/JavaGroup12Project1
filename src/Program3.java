4a6bee87369ed1358ca07aeaeabf58435eb7e16f
laurakyarova <laurakyarova@gmail.com>
2023-08-24 13:51:27 -0400

    Program3

--- src/Program3.java
+++ src/Program3.java
@@ -1,7 +1,18 @@
 public class Program3 {
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        int [][]numbers={
+                {12,3,56,78,4},
+                {10,23,8,15,7},
+                {21,11,14,6,2}};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                if(numbers[i][j]%2==0){
+                    System.out.print(numbers[i][j]+" ");
+                }
+
+            }
+            System.out.println();
 
     }
-}
+}}
