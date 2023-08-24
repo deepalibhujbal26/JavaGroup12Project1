c390fc9a2d8abed24ad706f8f934d17a8962ae6b
tahmingithub <tahminnka@gmail.com>
2023-08-24 14:36:43 -0400

    Task7

---
+++ src/Task7.java
@@ -0,0 +1,18 @@
+public class Task7 {
+    public static void main(String[] args) {
+
+        int n1 = 0;
+        int n2 = 1;
+
+        System.out.println("Fibonacci Series:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(n1 + " ");
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+
+
+
+    }
+}
--- src/program7.java
+++
@@ -1,8 +0,0 @@
-public class Task7 {
-    public static void main(String[] args) {
-
-
-        int age=18;
-        System.out.println("you are 18");
-    }
-}
